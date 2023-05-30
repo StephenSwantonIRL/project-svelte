93a95384ce5960416e0ae62a7bd7e840744d1709
Stephen <stephenswanton@gmail.com>
2023-05-30 16:21:04 +0100

    adds flowbite for svelte

--- src/pages/Index.svelte
+++ src/pages/Index.svelte
@@ -3,7 +3,13 @@
     import HomeContent from "../../components/HomeContent.svelte";
     import JoinCode from "../../components/JoinCode.svelte";
     import Footer from "../../components/Footer.svelte";
+    import { Alert } from "flowbite-svelte";
 </script>
+<div class="p-8">
+    <Alert>
+        <span class="font-medium">Info alert!</span> Change a few things up and try submitting again.
+    </Alert>
+</div>
 <JoinCode />
 <WelcomeMenu />
 <HomeContent />
