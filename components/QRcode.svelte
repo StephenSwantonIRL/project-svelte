3016318104c1afaba2aef07a1cd2f9955a283389
Stephen <stephenswanton@gmail.com>
2023-08-22 07:36:58 +0100

    adds qr code

---
+++ components/QRcode.svelte
@@ -0,0 +1,23 @@
+<script>
+    import "qrious"
+    import {onMount} from "svelte";
+    export let destination
+    export const getQRscript = async (destination) => {
+        const script = document.createElement("script");
+        script.src = "https://ss-ll.s3.eu-west-1.amazonaws.com/qrious.min.js";
+        document.head.appendChild(script);
+
+        script.onload = () => {
+            var qr = new QRious({
+                element: document.getElementById("qr"),
+                value: destination
+            });
+        }
+    }
+    onMount(() => {
+        getQRscript(destination)
+    })
+</script>
+<div>
+    <canvas id="qr"></canvas>
+</div>
--- components/SessionSettings.svelte
+++ components/SessionSettings.svelte
@@ -1,6 +1,7 @@
 <script>
     import {getContext} from "svelte";
     import Toggle from "./Toggle.svelte";
+    import QRcode from "./QRcode.svelte";
 
     const backEndService = getContext("BackEndService")
 
@@ -14,9 +15,11 @@
     } else {
         trueFalse = true
     }
+    let destination = "http://localhost:3001/#/"+shortcode
 
     async function updateShortCode(shortcodeValue, sessionId){
         await backEndService.assignShortCode(shortcodeValue, sessionId)
+        destination = "http://localhost:3001/#/"+shortcode
     }
 
     async function updateWaitMessage(waitMessageValue, sessionId){
@@ -40,6 +43,9 @@
         <h3 class="text-4xl mb-2 leading-tight font-semibold font-heading">Settings</h3>
         <div style="width:100%">
             Short Code: <input class="cs-text-field" style="width:50%" bind:value={shortcode} type="text" placeholder="xys" onfocusout={updateShortCode(shortcode, session.sessionid)}>
+            {#if shortcode}
+                <QRcode bind:destination={destination} />
+            {/if}
         </div>
         <div>
             Waiting Page Message: <textarea class="cs-text-field" style="width:50%" bind:value={waitMessage} placeholder="xys" onfocusout={updateWaitMessage(waitMessage, session.sessionid)} />
